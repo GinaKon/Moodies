b951e6d71149d23da9b16cc33c00b835f24c6a41
Gina Kondyleniou <128517967+GinaKon@users.noreply.github.com>
2023-12-12 21:42:24 +0000

    annoying pair programming

--- main.py
+++ main.py
@@ -1,16 +1,36 @@
-from random import choice
+# class Movie:
+#    def __init__(self, title, mood):
+#       self.title = title
+#       self.mood = mood
 
-#Creating a list of fav movies
-movies = [['Eat, Pray, Love', 'Joker', 'Cruella'], 
-          ['Cinema Paradiso', 'La Vita e Bella','Gomorrah'],
-           ['Dogtooth', 'Parasites', 'Old Boy'] ]
+#    def get_choice(self, movie_title):
+#       self.movie_title = movie_title
 
-#input mood
-print('What mood are you in?')
-mood = input()
+Moviesdict = [{'Title': 'Dogtooth','mood': "sceptical"},
+      {'Title':'Eat, Pray, Love', 'mood':'romantic'},
+   {'Title':'Joker', 'mood':'depressed'},
+    {'Title':'Cruella', 'mood':'powerful'}
+]
 
-#loop through and find a matching mood
+def MatchMood (inputmood):
+   for item in Moviesdict:
+    if  inputmood == (item['mood']):
+      return (item['Title'])
+   return "None"
+   
+   
 
-for item in movies:
-    if item[1] == mood:
-        print(mood + 'movie: ' + item[0])
+# Dogtooth = Movie ("Dogtooth", "sceptical")
+# EatPrayLove = Movie("Eat,Pray, Love", "romantic")
+# Joker = Movie ("Joker", "depressed")
+# Cruella = Movie ("Cruella", "powerful")
+# CinemaParadiso = Movie ("Cinema Paradiso", "emotional")
+# LaVitaeBella = Movie ("LaVita e Bella", "general")
+# Gomorrah = Movie ("Gomorrah", "sadistic")
+# Parasites = Movie ("Parasites", "analytical")
+# OldBoy    = Movie ("Old Boy", "spicy")
+# Elemental = Movie ("Elemental", "childish")
+
+
+Mood = input('How are you feeling today?')
+print(MatchMood(Mood.strip()))
